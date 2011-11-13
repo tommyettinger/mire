cf3edfe57149de1f4333623a96d56013178eec03
Tommy Ettinger <tommy.ettinger@gmail.com>
2011-11-13 12:43:16 -0800

    Upgrading to Clojure 1.3.0 .

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject mire "0.13"
   :description "A multiuser text adventure game/learning project."
   :main mire.server
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojars.arohner/clojure-contrib "1.2.0-equiv-SNAPSHOT"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]])
+(comment [org.clojure/clojure-contrib "1.2.0"])
